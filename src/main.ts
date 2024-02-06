ec45fd828d701e928445982618356e697003f08a
Dat Dao <dat.dao@c2c-techhub.io>
2024-02-06 11:57:54 +0700

    example tracing

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { otelSDK } from './tracing';
 
 async function bootstrap() {
+  // Start SDK before nestjs factory create
+  await otelSDK.start();
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
